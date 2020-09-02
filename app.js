bb424d8766e3541e609b5b72608fb7cb315d9c10
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-09-02 18:37:28 +0000

    add env package

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
@@ -19,9 +20,10 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-const secret = "Thisismyscretkey.";
-
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+userSchema.plugin(encrypt, {
+  secret: process.env.SECRET,
+  encryptedFields: ["password"]
+});
 
 const User = mongoose.model("User", userSchema);
 
